cc7c636a387343a5c7b6384eb1e4fbce585cc74b
praveen-990 <146806793+praveen-990@users.noreply.github.com>
2023-10-09 11:05:35 +0530

    Create add.c
    addition of two numbwers

---
+++ add.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+void main()
+{
+  int a,b,res;
+  printf("Enter two numbers : " );
+  scanf("%d%d",&a,&b);
+  res=a+b;
+  printf("Addition of two numbers %d and %d is %d",a,b,res);
+}
